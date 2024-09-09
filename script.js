420d0b14654a672385d24c966f5cdf83e1fef130
Sendi <sendikaddiwaemilly@gmail.com>
2024-09-09 22:30:25 +0300

    added creditsales

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const branch = document.getElementById('branch').value;
 
-    if (username === 'owner' && password === 'ownerpass') {
+    if (username === 'owner' && password === 'ownerpass'&& branch==='Matugga') {
         window.location.href = 'owner.dashboard.html';
-    } else if (username === 'salesagent' && password === 'agentpass') {
+    } else if (username === 'salesagent' && password === 'agentpass'&& branch==='Matugga') {
         window.location.href = 'sales.agent.dashboard.html';
-    } else if (username === 'manager' && password === 'managerpass') {
+    } else if (username === 'manager' && password === 'managerpass'&& branch==='Matugga') {
         window.location.href = 'manager_dashboard.html';
     } else {
         alert('Invalid username or password!');
     }
-});
+});       
